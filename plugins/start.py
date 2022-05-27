82e5f1129caf01f9793a1b8f3145b9d6602563f1
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-05-27 16:03:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,6 +123,6 @@ async def start(bot, message):
         await sts.delete()
         await bot.send_message(
             chat_id=message.chat.id,
-            text=f"😎 All files have been successfully sent to TARGET CHANNEL, If not sent check your logs.\n\nTotal Files:- sO0N\nForwarded:- <code>{frwded}</code>"
+            text=f"😎 All files have been successfully sent to TARGET CHANNEL, If not sent check your logs.\n\nTotal Files:- {totalfiles}\nForwarded:- <code>{frwded}</code>"
             )
         return
