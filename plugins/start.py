e46be91317b6244ddbac81c83ea100f01ef992ab
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-05-27 14:16:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -65,7 +65,9 @@ async def start(bot, message):
         pre = ""
     if data.split("-", 1)[0] == "BATCH":
         sts = await message.reply("🙂 I am sending files in your TARGET CHANNEL, when it will complete i will notify you via a message. If i am not sending files in your TARGET CHANNEL then check your logs.")
-        file_id = data.split("-", 1)[1]
+        #file_id = data.split("-", 1)[1]
+        file = data.split("-", 1)[1]
+        totalfiles, file_id = file.split("#", 1)
         msgs = INDEX_FILES.get(file_id)
         frwded = 0
         pling = 0
