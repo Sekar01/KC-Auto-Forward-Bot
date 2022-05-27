211101888ba97ea8514280482ee591868f6a95bc
KC Khais <khaiskc@gmail.com>
2022-05-27 20:22:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -133,11 +133,11 @@ async def start(bot, message):
                     )
                 frwded += 1
                 pling += 1
-                tot=totalfiles-frwded
             except Exception as e:
                 logger.warning(e, exc_info=True)
                 continue
             if pling == 1:
+                tot=totalfiles-frwded
                 await sts.edit_text(f"**__Now I'm forwarding file into target channel.__**\n\n**Forwarded:-** <code>{frwded}</code>\n\n**Total Files :-** <code>{tot}</code>\n\n⌚ Time :- {time_data(time.time())}")
                 pling -= 1
             await asyncio.sleep(3)
