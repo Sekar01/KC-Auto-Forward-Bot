3719a008c1ddce0cfe661b4a19dc7f9658c5967e
KC Khais <khaiskc@gmail.com>
2022-05-27 12:13:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -121,6 +121,6 @@ async def start(bot, message):
         await sts.delete()
         await bot.send_message(
             chat_id=message.chat.id,
-            text=f"😎 All files have been successfully sent to TARGET CHANNEL, If not sent check your logs.\n\nForwarded:- {frwded}"
+            text=f"😎 All files have been successfully sent to TARGET CHANNEL, If not sent check your logs.\n\nTotal Forwarded:- <code>{frwded}</code>"
             )
         return
